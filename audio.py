cf105666ec1760c0f9893bb76d2d1ab326a22afd
tim wang <timwang1550@gmail.com>
2021-05-02 22:41:02 -0700

    improved scale of audio bins

--- audio.py
+++ audio.py
@@ -1,7 +1,6 @@
 """
-Main file for recording audio input and data parsing
-Fourier Transform and moving peaks to audio bins
-for displaying
+main file for recording audio input, data parsing
+via an FFT, and rellocating peak values
 """
 
 import numpy as np
@@ -10,11 +9,10 @@ import pyaudio
 import serial
 import time
 
-
 # pyaudio constants
 FORMAT = pyaudio.paInt16
-CHUNK = 512    # number of samples taken
-DEVICE_NAME = 'MacBook Pro Microphone'
+CHUNK = 1024    # number of samples taken
+DEVICE_NAME = 'VB-Cable'
 
 # Arduino Port Constants
 PORT = '/dev/cu.usbmodem14101'
@@ -22,28 +20,25 @@ BAUD_RATE = 19200
 
 # audio visualizer constants
 BIN_COUNT = 8
-BIN_HEIGHT = 7
-LED_COUNT = 56
-
 
 def main():
     # instantiate pyaudio module
     p = pyaudio.PyAudio()
 
     # instantiate serial connection to arduino
-    connection = serial.Serial(PORT, BAUD_RATE)
-    print('\nport used:', connection, '\n')
+    #connection = serial.Serial(PORT, BAUD_RATE)
+    #print('\nport used:', connection, '\n')
 
     # select relevant params matching specified DEVICE_NAME
+    device_index = None
     max_channels = None
     default_rate = None
-    device_index = None
     for index in range(p.get_device_count()):
         device = p.get_device_info_by_index(index)
         if device.get('name') == DEVICE_NAME:
+            device_index = device.get('index')
             max_channels = device.get('maxInputChannels')
             default_rate = int(device.get('defaultSampleRate'))
-            device_index = device.get('index')
 
     # if device found matching DEVICE_NAME
     if device_index is not None:
@@ -70,38 +65,36 @@ def main():
                 # convert frequency peaks to audio bins 
                 # use only first half of data to ignore negative values
                 # and ignoring first bin full of DC gain
-                bins = [0] * BIN_COUNT   # clear bins
+                bins = [0] * BIN_COUNT   # reset bins
                 for i in range(1,(int(CHUNK/2))): 
-                    if transform_data[i] > 300:
-                        if (i <= 3):
-                            bins[0] += transform_data[i]
-                        elif (i>3 and i<=6):
-                            bins[1] += transform_data[i]
-                        elif (i>6 and i<=13):
-                            bins[2] += transform_data[i]
-                        elif (i>13 and i<=27):
-                            bins[3] += transform_data[i]
-                        elif (i>27 and i<=55):
-                            bins[4] += transform_data[i]
-                        elif (i>55 and i<=112):
-                            bins[5] += transform_data[i]
-                        elif (i>112 and i<=229):
-                            bins[6] += transform_data[i]
-                        elif (i > 229):
-                            bins[7] += transform_data[i]
+                    if (i <= 2):
+                        bins[0] += transform_data[i]
+                    elif (i>2 and i<=4):
+                        bins[1] += transform_data[i]
+                    elif (i>4 and i<=8):
+                        bins[2] += transform_data[i]
+                    elif (i>8 and i<=17):
+                        bins[3] += transform_data[i]
+                    elif (i>17 and i<=33):
+                        bins[4] += transform_data[i]
+                    elif (i>33 and i<=66):
+                        bins[5] += transform_data[i]
+                    elif (i>66 and i<=132):
+                        bins[6] += transform_data[i]
+                    elif (i > 132):
+                        bins[7] += transform_data[i]
                 
                 # send bin values to arduino
-                for pos in range(BIN_COUNT):
-                    connection.write(bytes([int(bins[pos]/100000)]))
+                #for pos in range(BIN_COUNT):
+                #    connection.write(bytes([int(bins[pos]/100000)]))
 
-                """
-                # display bins in numpy
+                #display bins in numpy
                 x_axis = np.arange(0,len(bins))
                 plt.bar(x_axis,bins)
                 x1,x2,_,_ = plt.axis()  
-                plt.axis((x1,x2,0,500000))
+                plt.axis((x1,x2,0,10_000_000))
                 plt.pause(.01)
-                plt.close() """
+                plt.close()
 
                 time.sleep(.01)
 
@@ -115,8 +108,7 @@ def main():
         p.terminate()
 
         # end serial connection
-        connection.close()
-
+        #connection.close()
 
 
 if __name__ == '__main__':
